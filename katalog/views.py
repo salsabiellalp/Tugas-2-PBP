265b1329bca5d01936cf3ef37d01b8f359a7405a
Bellalpsb <salsabiella4@gmail.com>
2022-09-10 14:17:08 +0700

    create views

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_wishlist = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Bella'
+    }
+    return render(request, "katalog.html", context)
